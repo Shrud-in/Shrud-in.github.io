370642c7c78bbc8e788bd274cf74b45782201ae0
Shrudin Suresh <92779132+Shrud-in@users.noreply.github.com>
2023-11-25 20:58:24 +0530

    modified

--- script.js
+++ script.js
@@ -12,50 +12,35 @@ document.addEventListener('DOMContentLoaded', function() {
     }, 3000); // Simulating 3 seconds of loading time
   
     function createElements(container, className, quantity) {
-        for (let i = 0; i < quantity; i++) {
-          const element = document.createElement('div');
-          element.className = className;
-          element.style.top = `${Math.random() * 100}vh`;
-          element.style.left = `${Math.random() * 100}vw`;
-          container.appendChild(element);
-        }
+      for (let i = 0; i < quantity; i++) {
+        const element = document.createElement('div');
+        element.className = className;
+        element.style.top = `${Math.random() * 100}vh`;
+        element.style.left = `${Math.random() * 100}vw`;
+        container.appendChild(element);
       }
-
-      createElements(confettiContainer, 'confetti', 100);
-      createElements(balloonsContainer, 'balloons', 50);
-    
+    }
+  
+    const confettiQuantity = confettiContainer.getAttribute('data-confetti') || 0;
+    const balloonsQuantity = balloonsContainer.getAttribute('data-balloons') || 0;
+  
+    createElements(confettiContainer, 'confetti', confettiQuantity);
+    createElements(balloonsContainer, 'balloons', balloonsQuantity);
+  
     // Define the redirectTo function
     function redirectTo(link) {
-        const links = {
-          link1: 'https://www.youtube.com/watch?v=l5LTjJ5Fs3Y&pp=ygUsc2VuZCB0aGlzIHRvIHlvdXIgZ2lybGZyaWVuZCBvbiBoZXIgYmlydGhkYXk%3D',
-          link2: 'https://example.com/gift2',
-          link3: 'https://example.com/gift3',
-          link4: 'https://example.com/gift4',
-          link5: 'https://example.com/gift5',
-          link6: 'https://example.com/gift6',
-        };
-
-
+      const links = {
+        link1: 'https://www.youtube.com/watch?v=l5LTjJ5Fs3Y&pp=ygUsc2VuZCB0aGlzIHRvIHlvdXIgZ2lybGZyaWVuZCBvbiBoZXIgYmlydGhkYXk%3D',
+        link2: 'https://example.com/gift2',
+        link3: 'https://example.com/gift3',
+        link4: 'https://example.com/gift4',
+        link5: 'https://example.com/gift5',
+        link6: 'https://example.com/gift6',
+      };
   
-      window.location.href = links[link] || '#';
       if (links.hasOwnProperty(link)) {
         window.location.href = links[link];
       }
     }
-  
-   
-  
-    for (let i = 0; i < 50; i++) {
-      const confetti = document.createElement('div');
-      confetti.className = 'confetti';
-      confettiContainer.appendChild(confetti);
-  
-      const balloon = document.createElement('div');
-      balloon.className = 'balloons';
-      balloonsContainer.appendChild(balloon);
-    }
-  
-    
-  
   });
   
