a3fcc911ed4a2aac61fb1616c8dfa3b8e429f8b1
Shrudin Suresh <92779132+Shrud-in@users.noreply.github.com>
2023-12-06 06:47:36 +0530

    allen

--- script.js
+++ script.js
@@ -4,92 +4,90 @@ document.addEventListener('DOMContentLoaded', function () {
   const confettiContainer = document.getElementById('confetti-container');
   const balloonsContainer = document.getElementById('balloons-container');
 
-  document.getElementById('buttons').addEventListener('click', function (event) {
-    if (event.target.tagName === 'BUTTON') {
-      redirectTo(event.target.getAttribute('data-link'));
-    }
-  });
-
+  // Show the loader for a few seconds
   setTimeout(() => {
-    loadingDiv.style.display = 'none';
-    mainContentDiv.style.animation = 'fade-in 2s ease';
+      loadingDiv.style.opacity = '0'; // Hide the loader
+      setTimeout(() => {
+          loadingDiv.style.display = 'none'; // Remove the loader from the DOM
+      }, 500); // Change the duration if needed
+      mainContentDiv.style.opacity = '1'; // Make the main content visible
   }, 3000);
 
-  function createConfetti(container, quantity) {
-    for (let i = 0; i < quantity; i++) {
-      const confetti = document.createElement('div');
-      confetti.className = 'confetti';
-      confetti.style.backgroundColor = getRandomColor();
-      container.appendChild(confetti);
-    }
+  // Only animate the heart during preloader
+  if (loadingDiv.style.opacity === '1') {
+      const heartElement = document.querySelector('.heart');
+      heartElement.style.animation = 'heartbeat 1s infinite';
   }
 
-  function createElements(container, className, quantity) {
-    for (let i = 0; i < quantity; i++) {
-      const element = document.createElement('div');
-      element.className = className;
-      element.style.top = `${Math.random() * 100}vh`;
-      element.style.left = `${Math.random() * 100}vw`;
-      container.appendChild(element);
-    }
-  }
+  createStaticShapes(document.querySelector('.wrapper'));
 
-  function createShapes(container) {
-    if (!container) {
-      return;
-    }
-
-    const shapes = [
-      { class: 'confetti-circle', color: '#d13447' },
-      { class: 'confetti-rectangle', color: '#ffbf00' },
-      { class: 'confetti-triangle', color: '#263672' },
-    ];
-
-    shapes.forEach(shapeData => {
-      const shape = document.createElement('div');
-      shape.className = shapeData.class;
-      shape.style.backgroundColor = shapeData.color;
-      container.appendChild(shape);
-    });
-  }
-
-  function getRandomColor() {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-  }
-
-  // ...
-
-  const confettiQuantity = confettiContainer.getAttribute('data-confetti') || 0;
-  const balloonsQuantity = balloonsContainer.getAttribute('data-balloons') || 0;
+  const confettiQuantity = parseInt(confettiContainer.getAttribute('data-confetti')) || 0;
+  const balloonsQuantity = parseInt(balloonsContainer.getAttribute('data-balloons')) || 0;
 
   createElements(confettiContainer, 'confetti', confettiQuantity);
   createElements(balloonsContainer, 'balloons', balloonsQuantity);
   createShapes(document.querySelector('.background-shapes'));
 
-  function redirectTo(link) {
-    const links = {
-      link1: 'https://www.youtube.com/watch?v=l5LTjJ5Fs3Y&pp=ygUsc2VuZCB0aGlzIHRvIHlvdXIgZ2lybGZyaWVuZCBvbiBoZXIgYmlydGhkYXk%3D',
-      link2: 'https://example.com/gift2',
-      link3: 'https://example.com/gift3',
-      link4: 'https://example.com/gift4',
-      link5: 'https://example.com/gift5',
-      link6: 'https://example.com/gift6',
-    };
+  // Additional code for confetti animation
+  createConfetti(confettiContainer, confettiQuantity);
 
-    if (links.hasOwnProperty(link)) {
-      window.location.href = links[link];
-    }
+  function createElements(container, className, quantity) {
+      for (let i = 0; i < quantity; i++) {
+          const element = document.createElement('div');
+          element.className = className;
+          container.appendChild(element);
+      }
   }
 
-  // Event listeners for button clicks
-  document.getElementById('buttons').addEventListener('click', function (event) {
-    if (event.target.tagName === 'BUTTON') {
-      redirectTo(event.target.getAttribute('data-link'));
-    }
-  });
+  function createShapes(container) {
+      if (!container) {
+          return;
+      }
+
+      const shapes = [
+          { class: 'confetti-circle', color: '#d13447' },
+          { class: 'confetti-rectangle', color: '#ffbf00' },
+          { class: 'confetti-triangle', color: '#263672' },
+      ];
+
+      shapes.forEach(shapeData => {
+          const shape = document.createElement('div');
+          shape.className = shapeData.class;
+          shape.style.backgroundColor = shapeData.color;
+          container.appendChild(shape);
+      });
+  }
+
+  function createConfetti(container, quantity) {
+      for (let i = 0; i < quantity; i++) {
+          const confetti = document.createElement('div');
+          confetti.className = 'confetti';
+          confetti.style.backgroundColor = getRandomColor();
+          container.appendChild(confetti);
+      }
+  }
+
+  function getRandomColor() {
+      const letters = '0123456789ABCDEF';
+      let color = '#';
+      for (let i = 0; i < 6; i++) {
+          color += letters[Math.floor(Math.random() * 16)];
+      }
+      return color;
+  }
+
+  function redirectTo(link) {
+      const links = {
+          link1: 'https://www.youtube.com/watch?v=l5LTjJ5Fs3Y&pp=ygUsc2VuZCB0aGlzIHRvIHlvdXIgZ2lybGZyaWVuZCBvbiBoZXIgYmlydGhkYXk%3D',
+          link2: 'https://example.com/gift2',
+          link3: 'https://example.com/gift3',
+          link4: 'https://example.com/gift4',
+          link5: 'https://example.com/gift5',
+          link6: 'https://example.com/gift6',
+      };
+
+      if (links.hasOwnProperty(link)) {
+          window.location.href = links[link];
+      }
+  }
 });
